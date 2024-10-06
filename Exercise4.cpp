c4ddd0209ffd9c1d4fa56f52f09c1492a476dcc6
ArdaBarak <79331834+ardabarak@users.noreply.github.com>
2024-10-05 21:12:49 -0400

    Lab 4 Exercise 4

--- Exercise4.cpp
+++ Exercise4.cpp
@@ -1,47 +1,25 @@
-// Arda Barak 
+// Arda Barak
 // 300129340
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
-//suits
-enum Suit {Clubs, Diamonds, Hearts, Spades};
-//ranks
-enum Rank {Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace};
-
-struct Card {
-    Rank rank;
-    Suit suit;
-};
-
-bool checkPair(Card hand[5]) {  //check for a pair
-    int rankCount[13] = {0};    //array for flag rank counts
-    for (int i = 0; i < 5; i++) 
-    {
-        rankCount[hand[i].rank - 2]++;
-    }
-    for (int i = 0; i < 13; i++) 
-    {
-        if (rankCount[i] == 2) { //check for pairs
-            return true;
+int uniqPathsNumber(int m, int n) { //grid of m*n
+    vector<int> vector(n, 1);
+    
+    for (int i = 1; i < m; i++) { //looping at every m to the right
+        for (int ix = 1; ix < n; ix++) {//loop again at n's to down
+            vector[ix] = vector[ix] + vector[ix - 1];
         }
-    }
-    return false;
+    }    
+    return vector[n-1];
 }
 
 int main() {
-    Card hand[5] = {
-        {Ace, Spades},
-        {Ace, Hearts},
-        {Jack, Clubs},
-        {Nine, Diamonds},
-        {Six, Diamonds}
-    };
-    
-    if (checkPair(hand)) {
-        cout << "We've a pair" << endl;
-    } else {
-        cout << "We've no pairs" << endl;
-    }
+    // int m = 3, n = 7; //28
+    // int m = 10, n = 10; //48620
+    int m = 4, n = 6; //56
+    cout << "Unique paths number is:    " << uniqPathsNumber(m, n) << endl;
     return 0;
 }
