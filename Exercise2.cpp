0f4f3b102c6cc6b591adc3634d480dede74ff8e6
ArdaBarak <79331834+ardabarak@users.noreply.github.com>
2024-10-05 21:11:56 -0400

    Lab 4 Exercise 2

--- Exercise2.cpp
+++ Exercise2.cpp
@@ -4,53 +4,104 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
-    int place;
-    Node* sonraki;
+class SetInt {
+private:
+    int* elem;// dynamic array's pointer
+    int size; // # of elems
+    bool containsAux(int n, int &poz) { //helper
+        for (int i = 0; i < size; i++) {
+            if (elem[i] == n) {
+                poz = i;
+                return true;
+            }
+        }
+        return false;
+    }
+
+public:
+
+    SetInt() : elem(nullptr), size(0) {}//default constr, empty set
+
+    SetInt(int array[], int arraySize) {//constr array &arraysize
+        size = arraySize;
+        elem = new int[size];
+        for (int i = 0; i < size; i++) {
+            elem[i] = array[i];
+        }
+    }
+
+    ~SetInt() {delete[] elem;} //destrucor
+
+    SetInt(const SetInt &other) { //copy constr
+        size = other.size;
+        elem = new int[size];
+        for (int i = 0; i < size; i++) {
+            elem[i] = other.elem[i];
+        }
+    }
+
+    void add(int n) { //adding an int
+        int poz;
+        if (!containsAux(n, poz)) { //check if int is already existent
+            int* newElem = new int[size + 1];
+            for (int i = 0; i < size; i++) {
+                newElem[i] = elem[i];
+            }
+            newElem[size] = n;  //add elem
+            delete[] elem;      //delete old array
+            elem = newElem;     //update pointer
+            size++;
+        }
+    }
+
+    void remove(int n) { //remove int
+        int poz;
+        if (containsAux(n, poz)) {
+            int* newElem = new int[size - 1];
+            for (int i = 0, j = 0; i < size; i++) {
+                if (i != poz) {
+                    newElem[j++] = elem[i];//copy everythign to remove(except elem)
+                }
+            }
+            delete[] elem;
+            elem = newElem;
+            size--;
+        }
+    }
+
+    bool contains(int n) { //check if a int is in the set
+        int poz;
+        return containsAux(n, poz);
+    }
+
+    int nbElem() {return size;} //return number of elems
+
+    int* tabElem() { //return a dynamic array of elems
+        if (size == 0) {
+            return nullptr;
+        }
+        int* array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = elem[i];
+        }
+        return array;
+    }
 };
 
-Node* yeniCircle(int N) { //circular linked list
-    Node* head = new Node;
-    head->place = 1;
-    Node* prev = head;
-
-    for (int i = 2; i <= N; i++) {  //rest of the nodes
-        Node* yeniNode = new Node;
-        yeniNode->place = i;
-        prev->sonraki = yeniNode;
-        prev = yeniNode;
-    }
-    //linking last nodes end back to the head
-    prev->sonraki = head;
-    return head;
-}
-
-int findLastChild(Node* head, int M) {
-    Node* current = head;
-    Node* prev = nullptr;
-
-    while (current->sonraki != current) {
-        for (int count = 1; count < M; count++) {
-            prev = current;
-            current = current->sonraki;
-        }
-        cout << "Removing kid number:   " << current->place << endl;
-        prev->sonraki = current->sonraki; //update prev nodes next pointer
-        delete current;         //delete the current node
-        current = prev->sonraki;//goto next child
-    }
-    return current->place;//last one returned
-}
-
 int main() {
-    int N, M;
-    cout << "input the total number of children:    ";
-    cin >> N;
-    cout << "input the number for M:                ";
-    cin >> M;
-    Node* head = yeniCircle(N);
-    int lastChild = findLastChild(head, M);
-    cout << "The last remaining child is at place: " << lastChild << endl;
-    delete head;
+    int array[] = {1, 2, 3, 4, 5};
+    SetInt set1(array, 5);
+    set1.add(6);//add elems
+    set1.add(2);//duplicate 2 testing
+    set1.remove(2);//remove 2
+    cout << "# of elems:    " << set1.nbElem() << endl;
+    int* elements = set1.tabElem();
+    if (elements) {
+        cout << "Elems list:    ";
+        for (int i = 0; i < set1.nbElem(); i++) {
+            cout << elements[i] << ", ";
+        }
+        delete[] elements;
+    }
     return 0;
 }
