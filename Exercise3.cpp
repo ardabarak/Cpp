899e21e8c8e5595d42641a1ebed7a72c760a1ff4
ArdaBarak <79331834+ardabarak@users.noreply.github.com>
2024-10-27 22:31:46 -0400

    Lab5 Exercise 3

--- Exercise3.cpp
+++ Exercise3.cpp
@@ -3,37 +3,70 @@
 
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <algorithm>
+#include <iomanip>
 
-int trapRainWater(vector<int>& height) {
-    int sol = 0; //left
-    int sag = height.size() - 1; //right
-    int solMax = 0, sagMax = 0; //left&right max
-    int trappedWater = 0;
+class SportsTeam {
+private:
+    std::string teamName;
+    int wins;
+    int loses;
+    int draws;
+    static int teamCount;
 
-    while (sol < sag) {
-        if (height[sol] < height[sag]) {
-            if (height[sol] >= solMax) {
-                solMax = height[sol]; //update left
-            } 
-            else {
-                trappedWater += solMax - height[sol]; //amount of trapped water
-            } sol++; //left pointer -> right
-        } 
-        else {
-            if (height[sag] >= sagMax) {
-                sagMax = height[sag]; //update right max
-            } else {
-                trappedWater += sagMax - height[sag]; //amount of trapped water
-            } sag--; //right pointer -> left
-        }
+public:
+    SportsTeam(std::string name, int w, int l, int d) : teamName(name), wins(w), loses(l), draws(d) {//constr
+        teamCount++;
     }
-    return trappedWater;
+    static int getTeamCount() {//get count
+        return teamCount;
+    }
+    double getWinPercentage() const {// get W%
+        int totalGames = wins + loses + draws;
+        return (totalGames > 0) ? (static_cast<double>(wins) / totalGames) * 100 : 0.0;
+    }
+    friend bool operator<(const SportsTeam& team1, const SportsTeam& team2);    //overloading <
+    friend bool operator>(const SportsTeam& team1, const SportsTeam& team2);    //overloading >
+
+    void displayStats() const { //team stats
+        std::cout << "Team          : " << teamName << ",\nWins          : " << wins << ",\nLoses         : " << loses << ",\nDraws         : " << draws << ",\nWin%          : " << std::fixed << std::setprecision(2) << getWinPercentage() << "\n" << std::endl;
+    }
+};
+
+int SportsTeam::teamCount = 0;
+
+bool operator<(const SportsTeam& team1, const SportsTeam& team2) {//overloading <
+    return team1.getWinPercentage() < team2.getWinPercentage();
+}
+bool operator>(const SportsTeam& team1, const SportsTeam& team2) {//overloading >
+    return team1.getWinPercentage() > team2.getWinPercentage();
 }
 
 int main() {
-    // vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1}; //6
-    vector<int> height = {0,1,0,2,1,1,1,2,2,2,3}; //4
-    cout << "Trapped water:     " << trapRainWater(height) << endl;
+    std::vector<SportsTeam> teams;
+    std::string name;
+    int wins;
+    int loses;
+    int draws;
+    int teamsNo;
+
+    std::cout << "Enter the # of teams: ";
+    std::cin >> teamsNo;
+    for (int i = 0; i < teamsNo; ++i) {
+        std::cout << "Enter name for team# " << i + 1 << ": ";
+        std::cin >> name;
+        std::cout << "Enter wins: ";
+        std::cin >> wins;
+        std::cout << "Enter lose: ";
+        std::cin >> loses;
+        std::cout << "Enter draw: ";
+        std::cin >> draws;
+        teams.emplace_back(name, wins, loses, draws);
+    }
+    std::sort(teams.begin(), teams.end(), std::greater<SportsTeam>());
+    std::cout << "Teams sorted by winning %:" << std::endl;
+    for (const auto& team : teams) {
+        team.displayStats();
+    }
     return 0;
 }
