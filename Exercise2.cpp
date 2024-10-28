8224e7008d9bd49562cf20432f0d5d74119fffb4
ArdaBarak <79331834+ardabarak@users.noreply.github.com>
2024-10-27 22:31:08 -0400

    Lab5 Exercise 2

--- Exercise2.cpp
+++ Exercise2.cpp
@@ -2,106 +2,67 @@
 // 300129340
 
 #include <iostream>
-using namespace std;
+#include <string>
+#include <vector>
 
-class SetInt {
+class User {
 private:
-    int* elem;// dynamic array's pointer
-    int size; // # of elems
-    bool containsAux(int n, int &poz) { //helper
-        for (int i = 0; i < size; i++) {
-            if (elem[i] == n) {
-                poz = i;
-                return true;
-            }
-        }
-        return false;
-    }
+    std::string username;
+    int userId;
+    std::string accessLevel;
+    static int userCount;
 
 public:
-
-    SetInt() : elem(nullptr), size(0) {}//default constr, empty set
-
-    SetInt(int array[], int arraySize) {//constr array &arraysize
-        size = arraySize;
-        elem = new int[size];
-        for (int i = 0; i < size; i++) {
-            elem[i] = array[i];
-        }
+    User(std::string username, int id, std::string acesslevel) : username(username), userId(id), accessLevel(acesslevel) { //constructor
+        userCount++;
     }
-
-    ~SetInt() {delete[] elem;} //destrucor
-
-    SetInt(const SetInt &other) { //copy constr
-        size = other.size;
-        elem = new int[size];
-        for (int i = 0; i < size; i++) {
-            elem[i] = other.elem[i];
-        }
+    ~User() { //destructor
+        userCount--;
     }
-
-    void add(int n) { //adding an int
-        int poz;
-        if (!containsAux(n, poz)) { //check if int is already existent
-            int* newElem = new int[size + 1];
-            for (int i = 0; i < size; i++) {
-                newElem[i] = elem[i];
-            }
-            newElem[size] = n;  //add elem
-            delete[] elem;      //delete old array
-            elem = newElem;     //update pointer
-            size++;
-        }
+    static int getUserCount() {//get user count
+        return userCount;
     }
+    friend bool compareAccess(const User& user1, const User& user2);//friend
+    friend std::ostream& operator<<(std::ostream& os, const User& user);
 
-    void remove(int n) { //remove int
-        int poz;
-        if (containsAux(n, poz)) {
-            int* newElem = new int[size - 1];
-            for (int i = 0, j = 0; i < size; i++) {
-                if (i != poz) {
-                    newElem[j++] = elem[i];//copy everythign to remove(except elem)
-                }
-            }
-            delete[] elem;
-            elem = newElem;
-            size--;
-        }
-    }
-
-    bool contains(int n) { //check if a int is in the set
-        int poz;
-        return containsAux(n, poz);
-    }
-
-    int nbElem() {return size;} //return number of elems
-
-    int* tabElem() { //return a dynamic array of elems
-        if (size == 0) {
-            return nullptr;
-        }
-        int* array = new int[size];
-        for (int i = 0; i < size; i++) {
-            array[i] = elem[i];
-        }
-        return array;
+    void displayInfo() const { //user info
+        std::cout << "Username: " << username << ", ID: " << userId << ", Access Level: " << accessLevel << std::endl;
     }
 };
 
+int User::userCount = 0;
+
+bool compareAccess(const User& user1, const User& user2) { //friend funct for two users
+    return user1.accessLevel == user2.accessLevel;
+}
+
+std::ostream& operator<<(std::ostream& os, const User& user) {
+    os << "(name)" << user.username << ", (id)" << user.userId << ", (access)" << user.accessLevel;
+    return os;
+}
+
 int main() {
-    int array[] = {1, 2, 3, 4, 5};
-    SetInt set1(array, 5);
-    set1.add(6);//add elems
-    set1.add(2);//duplicate 2 testing
-    set1.remove(2);//remove 2
-    cout << "# of elems:    " << set1.nbElem() << endl;
-    int* elements = set1.tabElem();
-    if (elements) {
-        cout << "Elems list:    ";
-        for (int i = 0; i < set1.nbElem(); i++) {
-            cout << elements[i] << ", ";
-        }
-        delete[] elements;
+    std::vector<User> users;
+    users.reserve(3);
+    for (int i = 0; i < 3; ++i) { //inputing users
+        std::string username, accessLevel;
+        int id;
+        std::cout << "\nEnter details for user " << i + 1 << ":\n";
+        std::cout << "Username      : ";
+        std::cin >> username;
+        std::cout << "ID            : ";
+        std::cin >> id;
+        std::cout << "Access Level  : ";
+        std::cin >> accessLevel;
+        users.emplace_back(username, id, accessLevel);
+        // User user(username, id, accessLevel);
     }
+
+    for (int i = 0; i < 3; i++) {
+        users[i].displayInfo();
+    }
+    std::cout << "Do " << users[0] << " and " << users[1] << " have the same access level? "  << (compareAccess(users[0], users[1]) ? "Yes" : "No") << std::endl;
+    std::cout << "Do " << users[1] << " and " << users[2] << " have the same access level? "  << (compareAccess(users[1], users[2]) ? "Yes" : "No") << std::endl;
+    std::cout << "Total users   : " << User::getUserCount() << std::endl;
     return 0;
 }
