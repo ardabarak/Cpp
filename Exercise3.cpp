9141f68ffbfc2641e3b047daf5a1165c85be753a
ArdaBarak <79331834+ardabarak@users.noreply.github.com>
2024-11-04 00:01:33 -0500

    Lab6 Exercise 3

--- Exercise3.cpp
+++ Exercise3.cpp
@@ -1,72 +1,65 @@
 // Arda Barak
 // 300129340
+// Exercise 3
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <string>
 #include <iomanip>
+using namespace std;
 
-class SportsTeam {
-private:
-    std::string teamName;
-    int wins;
-    int loses;
-    int draws;
-    static int teamCount;
+template <typename From, typename To>   //declaring base template 
+struct UnitConverter;
 
-public:
-    SportsTeam(std::string name, int w, int l, int d) : teamName(name), wins(w), loses(l), draws(d) {//constr
-        teamCount++;
-    }
-    static int getTeamCount() {//get count
-        return teamCount;
-    }
-    double getWinPercentage() const {// get W%
-        int totalGames = wins + loses + draws;
-        return (totalGames > 0) ? (static_cast<double>(wins) / totalGames) * 100 : 0.0;
-    }
-    friend bool operator<(const SportsTeam& team1, const SportsTeam& team2);    //overloading <
-    friend bool operator>(const SportsTeam& team1, const SportsTeam& team2);    //overloading >
-
-    void displayStats() const { //team stats
-        std::cout << "Team          : " << teamName << ",\nWins          : " << wins << ",\nLoses         : " << loses << ",\nDraws         : " << draws << ",\nWin%          : " << std::fixed << std::setprecision(2) << getWinPercentage() << "\n" << std::endl;
+template <>
+struct UnitConverter<double, double> {  //specializing convert m->cm
+    static double convert(double value, const string& fromUnit, const string& toUnit) {
+        if (fromUnit == "m" && toUnit == "cm") {
+            return value * 100.0;  //1m     = 100cm
+        } else if (fromUnit == "cm" && toUnit == "m") {
+            return value / 100.0;  //100cm  = 1m
+        } else {
+            return value;  //if units are the same
+        }
     }
 };
 
-int SportsTeam::teamCount = 0;
+template <>
+struct UnitConverter<int, int> {    //overloading for integer for int -> int
+    static int convert(int value, const string& fromUnit, const string& toUnit) {
+        if (fromUnit == "m" && toUnit == "cm") {
+            return value * 100;  //1m     = 100cm
+        } else if (fromUnit == "cm" && toUnit == "m") {
+            return value / 100;  //100cm  = 1m
+        } else {
+            return value;  //if units are the same
+        }
+    }
+};
 
-bool operator<(const SportsTeam& team1, const SportsTeam& team2) {//overloading <
-    return team1.getWinPercentage() < team2.getWinPercentage();
-}
-bool operator>(const SportsTeam& team1, const SportsTeam& team2) {//overloading >
-    return team1.getWinPercentage() > team2.getWinPercentage();
+template <typename T>
+void performConversion() {  //funct for user input and conversion
+    T value;
+    string fromUnit, toUnit;
+    cout << "Enter value to convert:    ";
+    cin >> value;
+    cout << "Convert From [cm, m]:      ";
+    cin >> fromUnit;
+    cout << "Convert To [cm, m]:        ";
+    cin >> toUnit;
+    T result = UnitConverter<T, T>::convert(value, fromUnit, toUnit);   //using UnitConverter struct for conversion
+    cout << fixed << setprecision(2);                                   //displaying results with 2 decimal places for floats
+    cout << value << " " << fromUnit << " is " << result << " " << toUnit << endl;
 }
 
 int main() {
-    std::vector<SportsTeam> teams;
-    std::string name;
-    int wins;
-    int loses;
-    int draws;
-    int teamsNo;
-
-    std::cout << "Enter the # of teams: ";
-    std::cin >> teamsNo;
-    for (int i = 0; i < teamsNo; ++i) {
-        std::cout << "Enter name for team# " << i + 1 << ": ";
-        std::cin >> name;
-        std::cout << "Enter wins: ";
-        std::cin >> wins;
-        std::cout << "Enter lose: ";
-        std::cin >> loses;
-        std::cout << "Enter draw: ";
-        std::cin >> draws;
-        teams.emplace_back(name, wins, loses, draws);
-    }
-    std::sort(teams.begin(), teams.end(), std::greater<SportsTeam>());
-    std::cout << "Teams sorted by winning %:" << std::endl;
-    for (const auto& team : teams) {
-        team.displayStats();
-    }
+    char choice;
+    cout << "Select input type [i for int, f for float]:    ";    //taking input as either integer or double
+    cin >> choice;
+    if (choice == 'i')          //if int
+        {performConversion<int>();} 
+    else if (choice == 'f')     //if float
+        {performConversion<double>();} 
+    else 
+        {cout << "Invalid input type " << endl;}
     return 0;
 }
