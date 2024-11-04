89def44e76b1240dc986bedbad96921eefe801fa
ArdaBarak <79331834+ardabarak@users.noreply.github.com>
2024-11-03 23:36:32 -0500

    Lab6 Exercise 2

--- Exercise2.cpp
+++ Exercise2.cpp
@@ -1,68 +1,29 @@
 // Arda Barak
 // 300129340
+// Exercise 2
 
 #include <iostream>
-#include <string>
-#include <vector>
+using namespace std;
 
-class User {
-private:
-    std::string username;
-    int userId;
-    std::string accessLevel;
-    static int userCount;
-
+template <typename T, int N> //using the given template
+class IterativeSquare {
 public:
-    User(std::string username, int id, std::string acesslevel) : username(username), userId(id), accessLevel(acesslevel) { //constructor
-        userCount++;
+    static T apply(T value) {
+        return applyOperation(value);
     }
-    ~User() { //destructor
-        userCount--;
-    }
-    static int getUserCount() {//get user count
-        return userCount;
-    }
-    friend bool compareAccess(const User& user1, const User& user2);//friend
-    friend std::ostream& operator<<(std::ostream& os, const User& user);
 
-    void displayInfo() const { //user info
-        std::cout << "Username: " << username << ", ID: " << userId << ", Access Level: " << accessLevel << std::endl;
+private:
+    static T applyOperation(T value) {
+        if constexpr (N > 0) {
+            return IterativeSquare<T, N - 1>::apply(value * value);
+        } else {
+            return value;
+        }
     }
 };
 
-int User::userCount = 0;
-
-bool compareAccess(const User& user1, const User& user2) { //friend funct for two users
-    return user1.accessLevel == user2.accessLevel;
-}
-
-std::ostream& operator<<(std::ostream& os, const User& user) {
-    os << "(name)" << user.username << ", (id)" << user.userId << ", (access)" << user.accessLevel;
-    return os;
-}
-
 int main() {
-    std::vector<User> users;
-    users.reserve(3);
-    for (int i = 0; i < 3; ++i) { //inputing users
-        std::string username, accessLevel;
-        int id;
-        std::cout << "\nEnter details for user " << i + 1 << ":\n";
-        std::cout << "Username      : ";
-        std::cin >> username;
-        std::cout << "ID            : ";
-        std::cin >> id;
-        std::cout << "Access Level  : ";
-        std::cin >> accessLevel;
-        users.emplace_back(username, id, accessLevel);
-        // User user(username, id, accessLevel);
-    }
-
-    for (int i = 0; i < 3; i++) {
-        users[i].displayInfo();
-    }
-    std::cout << "Do " << users[0] << " and " << users[1] << " have the same access level? "  << (compareAccess(users[0], users[1]) ? "Yes" : "No") << std::endl;
-    std::cout << "Do " << users[1] << " and " << users[2] << " have the same access level? "  << (compareAccess(users[1], users[2]) ? "Yes" : "No") << std::endl;
-    std::cout << "Total users   : " << User::getUserCount() << std::endl;
+    int result = IterativeSquare<int, 2>::apply(2);                     //computing (((2^2)^2)) from the example
+    cout << "Result of computing (((2^2)^2)) is:    " << result << endl;   //output must be 16
     return 0;
 }
