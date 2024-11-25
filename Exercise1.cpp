427da230fa1ce202b7af66cff1b2f535fa080beb
ArdaBarak <79331834+ardabarak@users.noreply.github.com>
2024-11-24 22:11:55 -0500

    Lab7&8 Exercise 1

--- Exercise1.cpp
+++ Exercise1.cpp
@@ -3,53 +3,41 @@
 // Exercise 1
 
 #include <iostream>
-#include <string>
-using namespace std;
 
-class Employee {                    //base class
-protected:
-    string name;
+class Date {
+private:
+    int day, month, year;
 public:
-    Employee(string empName) : name(empName) {}
-    virtual void calculateBonus() = 0;
-};
-
-class Manager : public Employee {   //derived Manager class
-    int projectsManaged;
-public:
-    Manager(string empName, int projects) : Employee(empName), projectsManaged(projects) {}
-
-    void calculateBonus() override {
-        int bonus = projectsManaged * 500;
-        cout << "Manager:   " << name << " has a bonus of:  $" << bonus << endl;
-    }
-};
-
-class Developer : public Employee { //derived Developer class
-    int linesOfCode;
-public:
-    Developer(string empName, int lines) : Employee(empName), linesOfCode(lines) {}
-
-    void calculateBonus() override {
-        int bonus = linesOfCode * 0.05;
-        cout << "Developer: " << name << " has a bonus of:  $" << bonus << endl;
-    }
+    Date(int d, int m, int y) : day(d), month(m), year(y) {} //date
+    void operator+(int n) {day += n;}                        //+ operator
+    void operator-(int n) {day -= n;}                        //- operator
+    //= operator
+    bool operator==(const Date& other) const {return year == other.year && month == other.month && day == other.day;}
+    //< operator
+    bool operator<(const Date& other) const {return year < other.year || (year == other.year && month < other.month) || (year == other.year && month == other.month && day < other.day);}
+    //> operator
+    bool operator>(const Date& other) const {return year > other.year || (year == other.year && month > other.month) || (year == other.year && month == other.month && day > other.day);}
+    void print() const {std::cout << day << "/" << month << "/" << year << std::endl;}
 };
 
 int main() {
-    string managerName, developerName;
-    int projects, lines;
-    cout << "Enter Manager's Name:  ";       //manager input
-    cin >> managerName;
-    cout << "Enter number of projects managed:  ";
-    cin >> projects;
-    Manager manager(managerName, projects);
-    cout << "\nEnter Developer's Name:  ";   //developer input
-    cin >> developerName;
-    cout << "Enter number of lines of code written: ";
-    cin >> lines;
-    Developer developer(developerName, lines);
-    manager.calculateBonus();               //calculating and showing results
-    developer.calculateBonus();
+    Date date1(15, 5, 2023);
+    Date date2(24, 6, 2024);
+    Date date3(31, 5, 2023);
+    Date date4(30, 5, 2023);
+    Date date5(20, 3, 2023);
+    date1.print();
+    date2.print();
+    date3.print();
+    date4.print();
+    date5.print();
+    date1 + 5;
+    date2 - 2;
+    date1.print();
+    date2.print();
+    std::cout << "is first date earlier than second : " << std::boolalpha << (date1 < date2) << std::endl;
+    std::cout << "is first date earlier than third  : " << std::boolalpha << (date1 < date3) << std::endl;
+    std::cout << "is second date later than third   : " << std::boolalpha << (date2 > date4) << std::endl;
+    std::cout << "is thirs date the same as fourth  : " << std::boolalpha << (date3 == date4) << std::endl;
     return 0;
 }
